a58bcf3ba6988518c60612f0c07148dfe8fb9ab1
Ryan Yang <ryany1819@gmail.com>
2019-10-20 13:21:50 -0700

    update

--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,5 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App.jsx'
 
-class App extends Component {
-	render() {
-		return (<div>Hello World!</div>);
-	}
-}
 ReactDOM.render(<App/>, document.getElementById('root'));
