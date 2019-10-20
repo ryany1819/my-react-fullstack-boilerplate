cc718575164096a53db8797972f075c308154306
Ryan Yang <ryany1819@gmail.com>
2019-10-20 11:43:46 -0700

    assets folder and working css webpack configuration

---
+++ src/client/components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<h1>Hello W!</h1>
+				<div id="panel1"></div>
+				<div id="panel2"></div>
+				<div id="panel3"></div>
+			</div>
+		);
+	}
+}
+
+export default App;
--- src/client/index.js
+++ src/client/index.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+import './assets/css/main.css';
 
-class App extends Component {
-	render() {
-		return (<div>Hello World!</div>);
-	}
-}
 ReactDOM.render(<App/>, document.getElementById('root'));
